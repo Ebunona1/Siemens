2aa7dd10a26c0009a2efcf52f0c0e0907fa668f8
Ole Ørke <ole.orke@hotmail.com>
2019-11-15 20:36:00 +0000

    add improved file with timestamps

--- Parser/parser.js
+++ Parser/parser.js
@@ -1,3 +1,12 @@
 var fs = require("fs")
-var temps = JSON.parse(fs.readFileSync("5s res sensor_id=BMS-L1O14S31&start=2017-03-20T00_00_00&end=2017-03-20T23_59_59.json"))
-console.log(temps)
+var data = JSON.parse(fs.readFileSync("AHU06 Heating Coil.json"))
+console.log(data)
+
+const items = data
+const replacer = (key, value) => value === null ? '' : value // specify how you want to handle null values here
+const header = Object.keys(items[0])
+let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+csv.unshift(header.join(','))
+csv = csv.join('\r\n')
+
+console.log(csv)
--- Parser/parser.py
+++ Parser/parser.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import json
 
-temps = pd.read_json("5s res sensor_id=BMS-L1O14S31&start=2017-03-20T00_00_00&end=2017-03-20T23_59_59.json")
-print(list(temps.keys()))
+with open("AHU06 Heating Coil.json") as f:
+    data = json.load(f)
+
+print(data[0])
